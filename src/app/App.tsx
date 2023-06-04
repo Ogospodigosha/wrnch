addc80e0ac4dffca72037f6778afbc8820bb6f0c
Ogospodigosha <odintsovis@hotmail.com>
2023-06-04 22:01:07 +0300

    add: Sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,17 +4,21 @@ import {Link, Route, Routes} from "react-router-dom";
 import {AddressPage} from "pages/AddressPage";
 import {MainPage} from "pages/MainPage";
 import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets/Sidebar";
 
 const App = () => {
     return (
         <div className='app'>
             <Navbar/>
-            <Suspense fallback={<div>loading...</div>} >
-                <Routes>
-                    <Route path={'/'} element={<MainPage/>}/>
-                    <Route path={'/address'} element={<AddressPage/>}/>
-                </Routes>
-            </Suspense>
+           <div className='content-page'>
+               <Sidebar/>
+               <Suspense fallback={<div>loading...</div>} >
+                   <Routes>
+                       <Route path={'/'} element={<div className='page-wrapper'><MainPage/></div>}/>
+                       <Route path={'/address'} element={<div className='page-wrapper'><AddressPage/></div>}/>
+                   </Routes>
+               </Suspense>
+           </div>
         </div>
     );
 };
